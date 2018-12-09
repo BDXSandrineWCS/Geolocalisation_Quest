b18f14fb1b520a049cbab9871d66909a1e66bb1f
sandrine <poupardsandrine@gmail.com>
2018-12-09 14:46:16 +0100

    Instancier un LocationManager step finished

--- app/src/main/java/fr/wcs/gps_quest/MainActivity.java
+++ app/src/main/java/fr/wcs/gps_quest/MainActivity.java
@@ -1,13 +1,54 @@
 package fr.wcs.gps_quest;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected LocationManager locationManager=null;
+    protected LocationListener locationListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initLocation();
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private void initLocation() {
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+
+        };
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,0,locationListener);
     }
 }
